5f0eca25042544a07a21f1197298e29c02432fef
Pooja Garva <poojagarva123@gmail.com>
2022-10-22 21:23:42 +0530

    learn module added

---
+++ src/common/Learn.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Learn.css";
+
+const Learn = () => {
+  return (
+    <a
+      href="https://drive.google.com/drive/folders/1TRlIYXPLPazrJb2lHQp5hmymqS4wj-Lx?usp=sharing"
+      target="_blank"
+    >
+      <div id="learn">
+        <i className="fa fa-book" aria-hidden="true"></i> <span>Learn</span>
+      </div>
+    </a>
+  );
+};
+
+export default Learn;
